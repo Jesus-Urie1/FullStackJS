446429a776367999591832bc917325edc1f6706a
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-09-26 12:27:09 -0500

    Eliminar paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -60,7 +60,24 @@ const actualizarPaciente = async (req, res) => {
   }
 };
 
-const eliminarPaciente = async (req, res) => {};
+const eliminarPaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
+
+  if (!paciente) {
+    res.status(404).json({ msg: "No encontrado" });
+  }
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: "Accion no válida" });
+  }
+
+  try {
+    await paciente.deleteOne();
+    res.json({ msg: "Paciente Eliminado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export {
   agregarPaciente,
