42d6c7d29089c80c48c506e08ff16d30657dfd39
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-06 21:04:29 -0500

    Guardando password modficado

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 import clientesAxios from "../config/axios";
 
@@ -7,6 +7,7 @@ const NuevoPassword = () => {
   const [password, setPassword] = useState("");
   const [alerta, setAlerta] = useState({});
   const [tokenValido, setTokenValido] = useState(false);
+  const [passwordModificado, setPasswordModificado] = useState(false);
 
   const params = useParams();
   const { token } = params;
@@ -29,6 +30,32 @@ const NuevoPassword = () => {
     comprobarToken();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      setAlerta({
+        msg: "El password de tener al menos 6 caracteres",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const url = `/veterinarios/olvide-password/${token}`;
+      const { data } = await clientesAxios.post(url, { password });
+      setAlerta({
+        msg: data.msg,
+      });
+      setPasswordModificado(true);
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
   const { msg } = alerta;
 
   return (
@@ -42,7 +69,7 @@ const NuevoPassword = () => {
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
         {msg && <Alerta alerta={alerta} />}
         {tokenValido && (
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Nuevo Password
@@ -62,6 +89,12 @@ const NuevoPassword = () => {
               />
             </div>
           </form>
+        )}
+
+        {passwordModificado && (
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            Iniciar Sesion
+          </Link>
         )}
       </div>
     </>
