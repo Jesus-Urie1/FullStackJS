9736fc04b8151a89cb1d599a777c44681013a469
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-06 20:43:10 -0500

    Validando token

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,9 +1,71 @@
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clientesAxios from "../config/axios";
 
 const NuevoPassword = () => {
-  return (
-    <div>NuevoPassword</div>
-  )
-}
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+  const [tokenValido, setTokenValido] = useState(false);
 
-export default NuevoPassword
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        await clientesAxios(`/veterinarios/olvide-password/${token}`);
+        setAlerta({
+          msg: "Coloca tu nuevo password",
+        });
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: "Hubo un error con el enlace",
+          error: true,
+        });
+      }
+    };
+    comprobarToken();
+  }, []);
+
+  const { msg } = alerta;
+
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Reestablece tu password y no Pierdas Acceso a tus
+          <span className="text-black"> Pacientes</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alerta alerta={alerta} />}
+        {tokenValido && (
+          <form>
+            <div className="my-5">
+              <label className="uppercase text-gray-600 block text-xl font-bold">
+                Nuevo Password
+              </label>
+              <input
+                type="password"
+                placeholder="Tu nuevo password"
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="submit"
+                value="Restablecer Password"
+                className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase 
+                    font-bold mt-5 hover:cursor-pointer hover:bg-indigo- md:w-auto "
+              />
+            </div>
+          </form>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NuevoPassword;
