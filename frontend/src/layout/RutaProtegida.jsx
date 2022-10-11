1641af0bf65ca3b0bdc67726555381c0397dd069
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-10 19:05:08 -0500

    Header

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -10,7 +10,13 @@ const RutaProtegida = () => {
   return (
     <>
       <Header />
-      {auth?._id ? <Outlet /> : <Navigate to="/" />}
+      {auth?._id ? (
+        <main className="container mx-auto mt-20">
+          <Outlet />
+        </main>
+      ) : (
+        <Navigate to="/" />
+      )}
       <Footer />
     </>
   );
