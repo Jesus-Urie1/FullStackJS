b1965e6a9aa417723f919894004de993f2678e0a
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-17 16:50:17 -0500

    Almacenar pacientes en la BD

--- frontend/src/context/PacientesProvider.jsx
+++ frontend/src/context/PacientesProvider.jsx
@@ -6,10 +6,29 @@ const PacientesContext = createContext();
 export const PacientesProvider = ({ children }) => {
   const [pacientes, setPacientes] = useState([]);
 
+  const guardarPaciente = async (paciente) => {
+    try {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await clientesAxios.post("/pacientes", paciente, config);
+      const { createdAt, updatedAt, __v, ...pacienteAlmacenado } = data;
+
+      setPacientes([pacienteAlmacenado, ...pacientes]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <PacientesContext.Provider
       value={{
         pacientes,
+        guardarPaciente,
       }}
     >
       {children}
