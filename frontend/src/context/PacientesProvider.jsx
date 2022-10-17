7d102d5edfe9ce1976b038d0b08cacb7b1383d79
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-17 18:00:28 -0500

    Componente Mostrar pacientes

--- frontend/src/context/PacientesProvider.jsx
+++ frontend/src/context/PacientesProvider.jsx
@@ -6,6 +6,27 @@ const PacientesContext = createContext();
 export const PacientesProvider = ({ children }) => {
   const [pacientes, setPacientes] = useState([]);
 
+  useEffect(() => {
+    const obtenerPacientes = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const { data } = await clientesAxios("/pacientes", config);
+        setPacientes(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerPacientes();
+  }, []);
+
   const guardarPaciente = async (paciente) => {
     try {
       const token = localStorage.getItem("token");
